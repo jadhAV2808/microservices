4f3b952b1fbb0f416632b1c6339d56cbe98d4598
jadhAv2808 <pbj8007@gmail.com>
2023-09-15 09:45:56 +0530

    started implementing okta

---
+++ api-gateway/src/main/java/com/microservice/controllers/AuthController.java
@@ -0,0 +1,55 @@
+package com.microservice.controllers;
+
+import org.slf4j.LoggerFactory;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.slf4j.Logger;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClient;
+import org.springframework.security.oauth2.client.annotation.RegisteredOAuth2AuthorizedClient;
+import org.springframework.security.oauth2.core.oidc.user.OidcUser;
+//import org.springframework.security.web.bind.annotation.AuthenticationPrincipal;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.microservice.models.AuthResponse;
+
+@RestController
+@RequestMapping("/auth")
+public class AuthController {
+	
+	private  Logger logger=LoggerFactory.getLogger(AuthController.class);
+	
+	
+	@GetMapping("/login")
+	public ResponseEntity<AuthResponse> login(@RegisteredOAuth2AuthorizedClient("okta") OAuth2AuthorizedClient client,
+		@AuthenticationPrincipal OidcUser user,
+		Model model){
+		
+		logger.info("User email id "+ user.getEmail());
+		
+		//creating auth res obj 
+		AuthResponse authRes= new AuthResponse();
+		authRes.setUserId(user.getEmail());
+		authRes.setAccessToken(client.getAccessToken().getTokenValue());
+		authRes.setRefreshToken(client.getRefreshToken().getTokenValue());
+		authRes.setExpireAt(client.getAccessToken().getExpiresAt().getEpochSecond());
+		
+		List<String>authorities= user.getAuthorities().stream().map(grantedAuthority -> {
+			return grantedAuthority.getAuthority();
+		}).collect(Collectors.toList());
+		
+		authRes.setAuthorities(authorities);
+		
+		
+		
+		return new ResponseEntity<>(authRes, HttpStatus.OK);
+	}
+
+}
